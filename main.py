0afad424f2839d147f5182b1941c5cca5d51672f
Chao Zhang <raidenchao@gmail.com>
2019-06-11 19:19:57 -0600

    update the git add position to [code] file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+import os
+
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
@@ -9,6 +11,32 @@ def home():
 @app.route("/about/")
 def about():
 	return render_template("about.html")
+
+@app.route("/image/")
+def image():
+	return render_template("image.html")
+
+@app.route("/music/")
+def music():
+    return render_template("music.html")
+
+@app.route("/game/")
+def game():
+	return render_template("game.html")
+
+# ///////////////////////////////////////////////////////////////////////// overwrite
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+# /////////////////////////////////////////////////////////////////////////
 
 if __name__ == '__main__':
 	app.run(debug = True)
