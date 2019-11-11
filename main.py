2b22ebe4e3e48276bd19da78a59c605bc86387b4
Chao Zhang <raidenchao@gmail.com>
2019-11-11 00:55:38 -0800

    finish open course presentation

--- main.py
+++ main.py
@@ -44,18 +44,22 @@ def courseCS():
 def presentation():
     return render_template("presentation.html")
 
-# ///////////////////////////////////////////////////////////////////////// overwrite
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
+@app.route("/openCourse/")
+def openCourse():
+    return render_template("openCourse.html")
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path, endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+# ///////////////////////////////////////////////////////////////////////// overwrite
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
+
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path, endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 # /////////////////////////////////////////////////////////////////////////
 
 if __name__ == '__main__':
