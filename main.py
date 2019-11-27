18e3fd371e0dacc2bcdd64f586820e5ffe0ffeeb
Chao Zhang <raidenchao@gmail.com>
2019-11-26 23:44:32 -0800

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for
 import os
 
 app = Flask(__name__, static_url_path='/static')
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def home():
@@ -49,17 +50,17 @@ def openCourse():
     return render_template("openCourse.html")
 
 # ///////////////////////////////////////////////////////////////////////// overwrite
-# @app.context_processor
-# def override_url_for():
-#     return dict(url_for=dated_url_for)
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
 
-# def dated_url_for(endpoint, **values):
-#     if endpoint == 'static':
-#         filename = values.get('filename', None)
-#         if filename:
-#             file_path = os.path.join(app.root_path, endpoint, filename)
-#             values['q'] = int(os.stat(file_path).st_mtime)
-#     return url_for(endpoint, **values)
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path, endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 # /////////////////////////////////////////////////////////////////////////
 
 if __name__ == '__main__':
